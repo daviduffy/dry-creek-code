35beaeb572447973980730b69f3c185ee4d72624
David Duffy <daviduffy1@gmail.com>
2025-10-24 14:48:18 -0700

    remove vue devtools from vite config to get build to work, maybe

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import yaml from 'vite-plugin-yaml2';
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    // vueDevTools(),
     yaml(),
   ],
   resolve: {
